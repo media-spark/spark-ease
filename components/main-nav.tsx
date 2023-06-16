493122063798d1c5b29e1ce7e795a94f1e33f120
Andy Randrianirina <andy.randrianirina@gmail.com>
2023-06-17 00:15:35 +0200

    Theme page and replace favicon

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -13,8 +13,8 @@ interface MainNavProps {
 export function MainNav({ items }: MainNavProps) {
   return (
     <div className="flex gap-6 md:gap-10">
-      <Link href="/" className="flex items-center space-x-2">
-        <Icons.logo className="h-6 w-6" />
+      <Link href="/" className="flex items-center">
+        <Icons.logo />
         <span className="inline-block font-bold">{siteConfig.name}</span>
       </Link>
       {items?.length ? (
--- config/site.ts
+++ config/site.ts
@@ -9,9 +9,16 @@ export const siteConfig = {
       title: "Home",
       href: "/",
     },
+    {
+      title: "Metadata",
+      href: "/",
+    },
+    {
+      title: "Publications",
+      href: "/",
+    },    
   ],
-  links: {
-    twitter: "https://twitter.com/shadcn",
+  links: {    
     github: "https://github.com/media-spark",
     docs: "https://www.notion.so/Media-Spark-Teamspace-2cdb6dc08f0548c9957a225715928a46?pvs=4",
   },
