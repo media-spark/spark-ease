2401fd5e623b7cade9d36c53e0f99f58335c5771
Andy Randrianirina <andy.randrianirina@gmail.com>
2023-06-17 00:31:21 +0200

    Fix header

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,6 @@ import { cn } from "@/lib/utils"
 import { SiteHeader } from "@/components/site-header"
 import { TailwindIndicator } from "@/components/tailwind-indicator"
 import { ThemeProvider } from "@/components/theme-provider"
-import Head from 'next/head'
 
 
 export const metadata: Metadata = {
@@ -35,7 +34,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <>
       <html lang="en" suppressHydrationWarning>
-        <Head />                
+        <head />                
         <body
           className={cn(
             "min-h-screen bg-background font-sans antialiased",
