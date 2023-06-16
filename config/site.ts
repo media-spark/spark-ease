88ffc34a2fe6604bc1926ba5d05cd6d170516872
Andy Randrianirina <andy.randrianirina@gmail.com>
2023-06-16 23:23:56 +0200

    Project Initialization with next, tailwind and ui components

---
+++ config/site.ts
@@ -0,0 +1,18 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "Spark Ease",
+  description:
+    "Content Management System (CMS), including features for video uploading, metadata management, and content publishing.",
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+  ],
+  links: {
+    twitter: "https://twitter.com/shadcn",
+    github: "https://github.com/media-spark",
+    docs: "https://www.notion.so/Media-Spark-Teamspace-2cdb6dc08f0548c9957a225715928a46?pvs=4",
+  },
+}
